bfbcb657d8bb5420b66d1c4e4bc1832717f8f9d9
Leonardo Souza <leonardonascimento@lampp-it.com.br>
2022-02-19 10:10:47 -0500

    base do projeto criada com conexão ao banco

---
+++ src/main/java/io/heart/developer/gastomicroservice/GastoMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package io.heart.developer.gastomicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastoMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GastoMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/heart/developer/gastomicroservice/GastoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.heart.developer.gastomicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastoMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
